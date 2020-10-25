6aff0bc8b83dcc5491ae61f6a60e5a7029678ff6
Väkiparta Eero <eero.vakiparta@student.samk.fi>
2020-10-25 22:41:32 +0200

    Send messages to db

--- src/App.js
+++ src/App.js
@@ -52,10 +52,38 @@ function ChatRoom() {
   //listen data with hook
   const [messages] = useCollectionData(query, { idField: "id" });
 
+  const [formValue, setFormValue] = useState("");
+
+  const sendMessage = async (e) => {
+    //prevent refresh
+    e.preventDefault();
+
+    const { uid, photoURL } = auth.currentUser;
+
+    //create new document in firestore
+    await messagesRef.add({
+      text: formValue,
+      createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+      uid,
+      photoURL,
+    });
+
+    setFormValue("");
+  };
+
   return (
     <div>
-      {messages &&
-        messages.map((msg) => <ChatMessage key={msg.id} message={msg} />)}
+      <div>
+        {messages &&
+          messages.map((msg) => <ChatMessage key={msg.id} message={msg} />)}
+      </div>
+      <form onSubmit={sendMessage}>
+        <input
+          value={formValue}
+          onChange={(e) => setFormValue(e.target.value)}
+        ></input>
+        <button type="submit">KISS</button>
+      </form>
     </div>
   );
 }
