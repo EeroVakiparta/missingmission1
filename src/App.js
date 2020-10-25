134a49f6d8ab0f26d259a73cfae661286c23d130
Väkiparta Eero <eero.vakiparta@student.samk.fi>
2020-10-25 22:34:40 +0200

    Chatmessage

--- src/App.js
+++ src/App.js
@@ -61,8 +61,14 @@ function ChatRoom() {
 }
 
 function ChatMessage(props) {
-  const { text, uid } = props.message;
-  return <p>{text}</p>;
+  const { text, uid, photoURL } = props.message;
+  const messageClass = uid === auth.currentUser.uid ? "sent" : "received";
+  return (
+    <div className={`message ${messageClass}`}>
+      <img src={photoURL} />
+      <p>{test}</p>
+    </div>
+  );
 }
 
 export default App;
